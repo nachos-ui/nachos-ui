69d744d5b9cb86c1884d7ee314a49c36e58b8f76
Petr Brzek <petrbrzek@abdoc.net>
2017-01-31 12:22:19 +0100

    edit code for highlighting doc examples

--- docs/assets/main.js
+++ docs/assets/main.js
@@ -1,1 +1,16 @@
-hljs.initHighlightingOnLoad(); // eslint-disable-line
+/* eslint-disable */
+
+var _highlight = function() {
+  setTimeout(function() {
+      var codeEl = [].slice.call(document.querySelectorAll('pre code'))
+      codeEl.forEach(function(code) {
+      hljs.highlightBlock(code)
+    })
+  })
+}
+
+window.addEventListener('load', function() {
+  _highlight()
+})
+
+_highlight()
